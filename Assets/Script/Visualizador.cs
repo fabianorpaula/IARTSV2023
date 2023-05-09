1bcb4bd6443da52eb4c4b92ded2d82226d6370f2
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-05-09 14:03:04 -0300

    Visualizador

---
+++ Assets/Script/Visualizador.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Visualizador : MonoBehaviour
+{
+    public TMP_Text populacao;
+    public TMP_Text comida;
+    public TMP_Text madeira;
+    public TMP_Text lazer;
+    public TMP_Text tipo;
+    public TMP_Text tempo;
+    //Onde Eu pego as informações
+    public Casa MinhaCasa;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        populacao.text = "População: " + MinhaCasa.Fazendeiros.Count + "/" + (MinhaCasa.QtdCasas * 5);
+        comida.text = "Comida: " + MinhaCasa.TotalComida;
+        madeira.text = "Madeira: " + MinhaCasa.TotalMadeira;
+        lazer.text = "Lazer: " + MinhaCasa.TotalVidaBoa;
+        //tipo;
+        //tempo;
+    }
+}
